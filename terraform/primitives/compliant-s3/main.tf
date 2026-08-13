d96e82a5dec089cf9a6b36a11bd1e8703c522c54
mark <janowski.mark.j@gmail.com>
2026-08-13 20:35:09 +0000

    unblock public acls as example

--- terraform/primitives/compliant-s3/main.tf
+++ terraform/primitives/compliant-s3/main.tf
@@ -105,7 +105,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "log" {
 
 resource "aws_s3_bucket_public_access_block" "log" {
   bucket                  = aws_s3_bucket.log.id
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
